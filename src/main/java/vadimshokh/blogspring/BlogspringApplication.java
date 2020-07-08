69b6aef8a1dbe1459be8a6751762ae44c002e4d1
Khimki-pro <vadimshoh@gmail.com>
2020-07-08 08:42:34 +0300

    Инициализировал проект

---
+++ src/main/java/vadimshokh/blogspring/BlogspringApplication.java
@@ -0,0 +1,13 @@
+package vadimshokh.blogspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vadimshokh/blogspring/BlogspringApplicationTests.java
@@ -0,0 +1,13 @@
+package vadimshokh.blogspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
