fdedb413680a48a688865bc8f3eafa4036b45a4d
Khimki-pro <vadimshoh@gmail.com>
2020-07-13 07:39:11 +0300

    Добавил простейшую реализацию фронта на vue

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,30 @@
+
+var messageApi = Vue.resource('/message{/id}');
+
+Vue.component('message-row', {
+  props: ['message'],
+  template: '<div><i>({{ message.id }})</i> {{ message.text }}</div>'
+});
+
+Vue.component('messages-list', {
+  props: ['messages'],
+  template:
+  '<div>' +
+        '<message-row v-for="message in messages" :key="message.id" :message="message" />' +
+  '</div>',
+  created: function() {
+    messageApi.get().then(result =>
+        result.json().then(data =>
+            data.forEach(message => this.messages.push(message))
+        )
+    )
+  }
+});
+
+var app = new Vue({
+  el: '#app',
+  template: '<messages-list :messages="messages"/>',
+  data: {
+    messages: []
+  }
+});
